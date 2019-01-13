6c34a140ec23cfe3af93d912a8d524a3af0b3f41
Guy Carmeli <guyc@wix.com>
2019-01-13 09:36:29 +0200

    Revert "Set elevation 0 when creating TopBar"
    This reverts commit 05dacbd0729f4ebf0074bd21c50f3bf813ad7fab.

--- lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/TopBarControllerTest.java
+++ lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/TopBarControllerTest.java
@@ -2,6 +2,7 @@ package com.reactnativenavigation.viewcontrollers;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import com.reactnativenavigation.BaseTest;
 import com.reactnativenavigation.mocks.TopBarBackgroundViewCreatorMock;
